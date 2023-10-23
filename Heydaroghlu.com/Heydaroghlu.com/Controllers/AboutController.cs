8ba66b885bf7350b3952a8125c5d53df16ecbb87
Heydaroghlu <heyderovisa777@gmail.com>
2023-10-24 02:02:58 +0400

    Start with Onion Architecture, Front to Back is ready

---
+++ Heydaroghlu.com/Heydaroghlu.com/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Heydaroghlu.com.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Heydaroghlu.com/Heydaroghlu.com/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Heydaroghlu.com.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
